640b3298c35bfb60cab602534053a2b0ee903036
Shaprizal Ibrahim <34850183+shaprizalibrahim@users.noreply.github.com>
2021-01-02 21:45:18 +0700

    Update header.php

--- 4/includes/header.php
+++ 4/includes/header.php
@@ -1,1 +1,41 @@
+<head>
 
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Dumb Hero</title>
+
+  <!-- Bootstrap CDN -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+  <!-- Custom styles for this template -->
+  <link href="style/features.css" rel="stylesheet">
+  <link href="style/style.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-secondary">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="index.php">Dumb Hero</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="addHero.php"><button class="btn btn-primary navbar-btn">Add Hero</button>
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="addRole.php"><button class="btn btn-primary navbar-btn">Add Role</button></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
