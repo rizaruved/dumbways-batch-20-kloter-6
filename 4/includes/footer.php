790577e7398189789e680f68654b15da4b2e23e8
Shaprizal Ibrahim <34850183+shaprizalibrahim@users.noreply.github.com>
2021-01-02 21:45:44 +0700

    Create footer.php

---
+++ 4/includes/footer.php
@@ -0,0 +1,13 @@
+<!-- Footer -->
+  <footer class="py-3 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Shaprizal Ibrahim 2020</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+
+</body>
