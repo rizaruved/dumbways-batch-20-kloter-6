90c0b4997e984303e828b604b86a5192bcf7c20e
Shaprizal Ibrahim <34850183+shaprizalibrahim@users.noreply.github.com>
2021-01-02 21:44:12 +0700

    Update dbConfig.php

--- 4/config/dbConfig.php
+++ 4/config/dbConfig.php
@@ -1,1 +1,9 @@
+<?php
+    //Koneksi ke database
+    $conn = mysqli_connect('localhost', 'root', '', 'dumb_hero');
 
+    //Cek koneksi
+    if(!$conn){
+        echo 'Connection error' . mysqli_connect_error();
+    }
+?>
