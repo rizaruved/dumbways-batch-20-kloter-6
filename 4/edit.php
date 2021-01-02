9a2da7edbb3c497cf34bc5c392c17e6224f2d6fa
Shaprizal Ibrahim <34850183+shaprizalibrahim@users.noreply.github.com>
2021-01-03 01:07:28 +0700

    Update edit.php

--- 4/edit.php
+++ 4/edit.php
@@ -1,35 +1,26 @@
 <?php
 
-include('config/dbConfig.php');
+  include('config/dbConfig.php');
 
-    // MENDAPATKAN ID TIAP POST
-    if(isset($_GET['id'])){
-        $id = $_GET['id'];
-        $sql = "SELECT * FROM `hero` WHERE id = $id";
-        $result = mysqli_query($conn, $sql);
-        $hero = mysqli_fetch_assoc($result);
-        mysqli_free_result($result);
-        mysqli_close($conn);
-    }
+    $id = $_GET['id'];
+    //Query untuk semua item di table heroes_tb
+    $sql = "SELECT * FROM `hero` AS h INNER JOIN `role` AS r ON h.id_role = r.role_id WHERE h.id = $id";
+    $sql2 = "SELECT * FROM role";
 
-    //SUBMIT
-    if(isset($_POST['submit'])) {
-        $name = $_POST['name'];
-        $role_id = $_POST['role_id'];
-        $description = $_POST['description'];
+    //Membuat query dan menyimpan hasilnya dalam variabel result
+    $result = mysqli_query($conn, $sql);
+    $result2 = mysqli_query($conn, $sql2);
 
-        $sql = "UPDATE hero SET name='$name', id_role='$role_id, description='$description' WHERE id=$id";
+    //Fetch hasil query sebagai associative array
+    $heroes = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    $heroes2 = mysqli_fetch_all($result2, MYSQLI_ASSOC);
 
-        //Menginput data form ke Database
-        if(mysqli_query($conn, $sql)){
-            //Berhasil
-            header('Location: index.php');
-        //Gagal
-        }else{
-            echo 'Query error: ' . mysqli_error($conn);
-        }
-    }
+    //Free Memory
+    mysqli_free_result($result);
+    mysqli_free_result($result2);
 
+    //Menutup koneksi
+    mysqli_close($conn);
 ?>
 
 <!DOCTYPE html>
@@ -46,23 +37,18 @@ include('config/dbConfig.php');
       <!-- INPUT NAMA HERO -->
       <div class="form-group">
         <label for="nama">Nama Hero:</label>
-        <input type="text" class="form-control" name="name" placeholder="<?php echo $hero['name'] ?>">
+        <input type="text" class="form-control" name="name" placeholder="<?php foreach($heroes as $hero1): echo $hero1['name']; endforeach; ?>">
       </div>
       
       <!-- SELECT ROLE HERO -->
       <div class="form-group">
-        <?php
-          $sql2 = "SELECT * FROM 'role' ";
-          $hasil = mysqli_query($conn, $sql2);
-          $selections = mysqli_fetch_all($hasil, MYSQLI_ASSOC);
-        ?>
         
         <label class="my-1 mr-2" for="role_id">Role Hero: </label>
         <!-- Role Selection -->
         <select class="custom-select my-1 mr-sm-2" name='role_id'>
-          <option selected>Role...</option>
-          <?php foreach($selections as $select): ?>
-            <option value="<?php echo $select['role_id'] ?>"><?php echo $select['role_name'] ?></option>
+          <option selected><?php foreach($heroes as $hero1): echo $hero1['role_name']; endforeach; ?></option>
+          <?php foreach($heroes2 as $hero): ?>
+            <option value="<?php echo $hero['role_id'] ?>"><?php echo $hero['role_name'] ?></option>
           <?php endforeach; ?>
         </select>
       </div>
@@ -70,11 +56,33 @@ include('config/dbConfig.php');
       <!-- DESKRIPSI HERO -->
       <div class="form-group">
         <label for="description">Deskripsi Hero:</label>
-        <textarea class="form-control" rows="3" name="description" placeholder="<?php echo $hero['description'] ?>"></textarea>
+        <textarea class="form-control" rows="3" name="description" placeholder="<?php foreach($heroes as $hero1): echo $hero1['description']; endforeach; ?>"></textarea>
       </div>
+
+      <br>
       
       <button type="submit" name="submit" value="submit" class="btn btn-primary mb-2">Submit</button>
     </form>
+
+    <?php 
+      //SUBMIT
+      if(isset($_POST['submit'])) {
+        $name = $_POST['name'];
+        $role_id = $_POST['role_id'];
+        $description = $_POST['description'];
+
+        $sql = "UPDATE hero SET name='$name', id_role='$role_id', description='$description' WHERE id = $id";
+
+        //Menginput data form ke Database
+        if(mysqli_query($conn, $sql)){
+            //Berhasil
+            header('Location: index.php');
+        //Gagal
+        }else{
+            echo 'Query error: ' . mysqli_error($conn);
+        }
+      }
+    ?>
 
   </div>
 
